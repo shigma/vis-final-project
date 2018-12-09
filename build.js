5dc4b4926f5c9ba4bb954d0299bc1089bed6949e
githubshn <js4418@126.com>
2018-12-09 15:16:09 +0800

    Update build.js

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const baseDir = path.resolve(__dirname, 'assets')
 
-fs.writeFileSync(path.resolve(__dirname, 'output.json'), JSON.stringify(
+fs.writeFileSync(path.resolve(__dirname, 'meta.json'), JSON.stringify(
   [].concat(...fs.readdirSync(baseDir)
     .map(name => fs.readFileSync(path.resolve(baseDir, name), 'utf8')
       .split(/(?=^From .+\r?\n(?:(?:From|Date|Subject|Message-ID|In-Reply-To|References): .+(\r?\n .+)*)+)/gmi)
@@ -11,7 +11,7 @@ fs.writeFileSync(path.resolve(__dirname, 'output.json'), JSON.stringify(
       .map(mail => {
         const data = {}
         const heading = mail.match(/^From .+((\r?\n.+)+)/)
-        data.text = mail.slice(heading[0].length).trim()
+        //data.text = mail.slice(heading[0].length).trim()
         data.meta = {}
         heading[1].match(/^(?:From|Date|Subject|Message-ID|In-Reply-To|References): .+(\r?\n .+)*/mg).forEach(meta => {
           const key = meta.match(/^[\w-]+/)[0]
