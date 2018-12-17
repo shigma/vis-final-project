fae4719b9591136968be0bb484305feefc0ec15e
githubshn <js4418@126.com>
2018-12-17 19:49:52 +0800

    attempt201812171949

---
+++ Vue_inreplyto.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+    
+<script>
+    const IRT = require('./dist/inreplytoTable.json');
+    module.exports = {
+        el : '#MailInReplyTo',
+        data: () => ({
+            SelectMailID : 2,
+            ParentMailID : IRT[SelectMailID].p,
+            InReplyToMailID : IRT[SelectMailID].irt_id
+        }),
+        methods: {
+            MainMail : function(){
+                return SelectMailID;
+            }
+        },
+        components: {
+            'SelectedMail': () => ({
+                template: '<h1> selected mail ID : {{ MainMail() }}</h1>'
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
+
