d1117aa736dbb80713a44d7475f5ea9873e0c33d
Hao He <hehao1998@pku.edu.cn>
2018-12-24 14:27:55 +0800

    Added Report Draft

---
+++ src/user_activity.vue
@@ -0,0 +1,18 @@
+<script>
+var app = new Vue ({
+    el: '#app',
+    data: {
+        message: 'Hello, Vue'
+    }
+})
+</script>
+
+<template>
+    <div id="app">
+        {{message}}
+    </div>
+</template>
+    
+<style lang="scss" scoped>
+
+</style>
