96ba9c75d642e13caced000a3f85ece13f78cb8a
Shigma <1700011071@pku.edu.cn>
2019-01-18 15:10:02 +0800

    fix activity plot
    1. fix activity plot brushing bug
    2. react plot on resizing

--- src/ActivityPlot.vue
+++ src/ActivityPlot.vue
@@ -14,141 +14,131 @@
  * @since 2019-01-07
  */
 
-const echarts = require("echarts");
-const eventBus = require("../src/EventBus.js");
+const eventBus = require('./EventBus')
+const { debounce } = require('throttle-debounce')
+
+const staticOptions = {
+    visualMap: {
+        show: false,
+        type: 'continuous',
+        seriesIndex: 0,
+        min: 0,
+    },
+    title: {
+        left: 'left',
+        text: 'Activity',
+    },
+    legend: {
+        left: 'center',
+        data: ['邮件数'],
+    },
+    tooltip: {
+        trigger: 'axis',
+    },
+    toolbox: {
+        feature: {
+            brush: {
+                type: ['lineX', 'clear'],
+            },
+        },
+    },
+    brush: {
+        xAxisIndex: 'all',
+        throttleType: 'debounce',
+        throttleDelay: 50000,
+        outOfBrush: {
+            colorAlpha: 0.1,
+        },
+    },
+    yAxis: {
+        splitLine: { show: false },
+    },
+    series: {
+        name: '邮件数',
+        type: 'line',
+        showSymbol: false,
+    },
+}
 
 module.exports = {
     data: () => ({
-        beginDate: Date,
-        endDate: Date
+        beginDate: null,
+        endDate: null,
     }),
     props: {
         data: {
             required: true,
-            type: Object
+            type: Object,
         },
         tag: {
-            type: String
-        }
-    },
-    mounted: function() {
-        this.setEchart();
+            type: String,
+        },
     },
     watch: {
-        originalData: function(newData, oldData) {
-            this.setEchart();
-        }
+        data: 'setOption',
     },
     computed: {
-        originalData() {
-            return this.data;
-        },
         maxValue() {
-            return this.originalData.reduce((total, curr) => {
+            return this.data.reduce((total, curr) => {
                 return total > curr[1] ? total : curr[1];
             });
         },
-        option() {
-            let dateList = this.originalData.map(function(item) {
-                return item[0];
-            });
+    },
+    mounted() {
+        this.chart = echarts.init(this.$el)
+        this.setOption()
+        this.chart.on('brush', debounce(100, params => {
+            // null if no range is chosen
+            this.beginDate = null;
+            this.endDate = null;
 
-            let valueList = this.originalData.map(function(item) {
-                return item[1];
-            });
-
-            let obj = {
-                // Make gradient line here
-                visualMap: {
-                    show: false,
-                    type: "continuous",
-                    seriesIndex: 0,
-                    min: 0,
-                    max: this.maxValue
-                },
-                title: {
-                    left: "left",
-                    text: "Activity"
-                },
-                legend:{
-                    left: "center",
-                    data: ["邮件数"],
-                },
-                tooltip: {
-                    trigger: "axis"
-                },
-                toolbox: {
-                    feature: {
-                        brush: {
-                            type: ["lineX", "clear"]
-                        }
-                    }
-                },
-                brush: {
-                    xAxisIndex: "all",
-                    throttleType: "debounce",
-                    throttleDelay: 50000,
-                    outOfBrush: {
-                        colorAlpha: 0.1
-                    }
-                },
-                xAxis: {
-                    data: dateList
-                },
-                yAxis: {
-                    splitLine: { show: false }
-                },
-                series: {
-                    name: "邮件数",
-                    type: "line",
-                    showSymbol: false,
-                    data: valueList
+            if (params.areas[0]) {
+                let range = params.areas[0].coordRange;
+                if (range[0] > 0 && range[0] < this.data.length) {
+                    this.beginDate = new Date(this.data[range[0]][0]);
                 }
-            };
-            return obj;
-        }
+                if (range[1] > 0 && range[1] < this.data.length) {
+                    this.endDate = new Date(this.data[range[1]][0]);
+                }
+            }
+
+            eventBus.$emit('date-filter-changed', {
+                beginDate: this.beginDate,
+                endDate: this.endDate,
+                tag: this.tag,
+            });
+        }))
+        eventBus.$on('resize', debounce(100, () => {
+            if (!this.chart) return
+            this.chart.resize()
+        }))
     },
     methods: {
-        setEchart() {
-            if (this.chart === undefined) {
-                let dom = this.$refs.activityplot;
-                this.chart = echarts.init(dom);
-                this.chart.on("brush", params => {
-                    // null if no range is chosen
-                    this.beginDate = null;
-                    this.endDate = null;
-                    //console.log(params);
+        setOption() {
+            if (!this.chart) return
+            this.chart.setOption({
+                ...staticOptions,
+                visualMap: {
+                    ...staticOptions.visualMap,
+                    max: this.maxValue,
+                },
+                xAxis: {
+                    data: this.data.map(item => item[0]),
+                },
+                series: {
+                    ...staticOptions.series,
+                    data: this.data.map(item => item[1]),
+                },
+            });
+        },
+    },
+}
 
-                    if (params.areas[0]) {
-                        let range = params.areas[0].coordRange;
-                        if (range[0] > 0 && range[0] < this.data.length) {
-                            this.beginDate = new Date(this.data[range[0]][0]);
-                        }
-                        if (range[1] > 0 && range[1] < this.data.length) {
-                            this.endDate = new Date(this.data[range[1]][0]);
-                        }
-                    }
-
-                    eventBus.$emit("date-filter-changed", {
-                        beginDate: this.beginDate,
-                        endDate: this.endDate,
-                        tag: this.tag
-                    });
-                });
-            }
-            this.chart.setOption(this.option);
-        }
-    }
-};
 </script>
 
 <template>
-    <div ref="activityplot"></div>
+    <div></div>
 </template>
 
 <style lang="scss" scoped>
-.echarts {
-    width: 100%;
-    height: 100%;
-}
 </style>
